f61635145b226b49f75f2e084f893f9b1fa0eed0
JJ Jiang <jingjiejiang85@gmail.com>
2019-05-19 15:55:28 +0800

    125. Valid Palindrome

--- LeetCode/String/ValidPalindrome.js
+++ LeetCode/String/ValidPalindrome.js
@@ -2,9 +2,23 @@
 
 /*
  * 
- * @jingjiejiang May 13, 2019
+ * @jingjiejiang May 19, 2019
  */ 
 var isPalindrome = function (s) {
 
+    let head = 0;
+    let rear = s.length - 1;
+    let regex = /[a-z0-9]/i;
 
+    while (head <= rear) {
+        if (!regex.test(s[head])) {
+            head++;
+        } else if (!regex.test(s[rear])) {
+            rear--;
+        } else if (s[head++].toLowerCase() !== s[rear--].toLowerCase()) {
+            return false;
+        }
+    }
+
+    return true;
 };
