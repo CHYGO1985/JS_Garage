dcc16cb9dffc31c42348fc1dadfb6b902b7d4c85
JJ Jiang <jingjiejiang85@gmail.com>
2019-05-22 16:14:16 +0800

    204. Count Primes

---
+++ LeetCode/Math/CountPrimes.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ * 
+ * @jingjiejiang May 22, 2019
+ */
+var countPrimes = function (n) {
+
+    let count = 0, isCount = [];
+
+    for (let i = 2; i < n; i++) {
+        if (isCount[i] === undefined) {
+            count++;
+            for (let j = i; i * j < n; j++) {
+                isCount[i * j] = true;
+            }
+        }
+    }
+
+    return count;
+};
