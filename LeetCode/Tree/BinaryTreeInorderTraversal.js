019145beae8af65886c2cf05214bed19dc67e4f8
JJ Jiang <jingjiejiang85@gmail.com>
2019-06-07 18:12:48 +0800

    94. Binary Tree Inorder Traversal

---
+++ LeetCode/Tree/BinaryTreeInorderTraversal.js
@@ -0,0 +1,37 @@
+
+ //Definition for a binary tree node.
+ function TreeNode(val) {
+     this.val = val;
+     this.left = this.right = null;
+ }
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ * 
+ * @jingjiejiang Jun 7, 2019
+ */
+var inorderTraversal = function (root) {
+
+    let stack = [];
+    let res = [];
+
+    if (root === null) return [];
+
+    const pushLeft = (root) => {
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+        return root
+    }
+
+    while (root || stack.length > 0) {
+        pushLeft(root);
+        let curNode = stack.pop();
+        res.push(curNode.val);
+        root = curNode.right;
+    }
+
+    return res;
+};
