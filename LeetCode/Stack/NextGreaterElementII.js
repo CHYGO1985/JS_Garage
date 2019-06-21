8003eb63f1ab1dc1247d0e1fd16d0dcbba7b304c
JJ Jiang <jingjiejiang85@gmail.com>
2019-06-21 13:35:41 +0800

    503. Next Greater Element II

--- LeetCode/Stack/NextGreaterElementII.js
+++ LeetCode/Stack/NextGreaterElementII.js
@@ -4,28 +4,24 @@
  * 
  * @jingjiejiang Jun 21, 2019
  * // further test [1,2,3,4,3]
+ * // https://leetcode.com/problems/next-greater-element-ii/discuss/98273/Java-10-lines-and-C%2B%2B-12-lines-linear-time-complexity-O(n)-with-explanation
  */
 var nextGreaterElements = function (nums) {
 
     let stack = [];
     let res = [];
-    let resIdx = 0;
     nums = nums.concat(nums)
 
     nums.forEach((num, idx) => {
-
-        
-        res[resIdx] = -1;
+    
+        res[idx] = -1;
 
         while (stack.length > 0 && num > nums[stack[stack.length - 1]]) {
-            res[--shiftIdx] = num;
+            res[stack[stack.length - 1]] = num;
             stack.pop();
         }
         stack.push(idx);
-        resIdx++;
     });
 
-    res.slice(res.length / 2);
-
-    return res;
+    return res.slice(0, Math.floor(res.length / 2));
 };
