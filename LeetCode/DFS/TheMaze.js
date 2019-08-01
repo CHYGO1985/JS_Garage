fe73a7072ffb8502ba574520cfa6475eda5abd4e
JJ Jiang <jingjiejiang85@gmail.com>
2019-08-01 19:46:05 +0800

    490. The Maze

--- LeetCode/DFS/TheMaze.js
+++ LeetCode/DFS/TheMaze.js
@@ -8,44 +8,40 @@
  */
 var hasPath = function(maze, start, destination) {
     const dirs = [[0, -1], [0, 1], [-1, 0], [1, 0]];
-    const steps = [];
-    steps.push(start);
-    
+    const status = maze.slice();
 
-    const isInRange = (loc, shift) => {
-        return loc[0] + shift[0] >= 0 && loc[0] + shift[0] < maze.length
-                && loc[1] + shift[1] >= 0 && loc[1] + shift[1] < maze[0].length
-                && maze[loc[0] + shift[0]][loc[1] + shift[1]] === 0
-                && maze[loc[0] + shift[0]][loc[1] + shift[1]] !== 2;
+    const isInRange = (shift) => {
+        return shift[0] >= 0 && shift[0] < maze.length
+                && shift[1] >= 0 && shift[1] < maze[0].length
+                && maze[shift[0]][shift[1]] !== 1;
     };
 
-    const dfs = (steps) => {
-
-        console.log(steps[steps.length - 1][0] + " " + destination[0] + "|" +
-                   steps[steps.length - 1][1] + " " + destination[1]);
-        
-
-        if (steps[steps.length - 1][0] === destination[0] && 
-            steps[steps.length - 1][1] === destination[1]) {
-            
+    const dfs = (preStart) => {
+        if (preStart[0] === destination[0] && preStart[1] === destination[1]) {
             return true;
         }
 
         for (let dirIdx = 0; dirIdx < dirs.length; dirIdx ++) {
-            const cur = steps[steps.length - 1];
-            maze[cur[0]][cur[1]] = 2;
-            if (isInRange(cur, dirs[dirIdx]) === false) continue;
-            
-            steps.push([cur[0] + dirs[dirIdx][0], cur[1] + dirs[dirIdx][1]]);
-            dfs(steps);
-            steps.pop();
+            /* shallow and deep copy cost me two hours */
+            let shift = preStart.slice();
+
+            while(isInRange(shift)) {
+                shift[0] += dirs[dirIdx][0];
+                shift[1] += dirs[dirIdx][1];
+            }
+        
+            shift[0] -= dirs[dirIdx][0];
+            shift[1] -= dirs[dirIdx][1];
+
+            if (status[shift[0]][shift[1]] === 2) continue;
+
+            // set point as visited
+            status[shift[0]][shift[1]] = 2;
+            if (dfs(shift)) return true;
         }
         
         return false;
     };
     
-    
-    const res = dfs(steps);
-    
-    return res;
+    return dfs(start);
 };
