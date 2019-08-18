959dac5bd57fa1438c235feabe5c1f339c0b3c56
JJ Jiang <jingjiejiang85@gmail.com>
2019-08-18 17:40:30 +0800

    450. Delete Node in a BST450. Delete Node in a BST

---
+++ LeetCode/Tree/DeleteNodeInABST.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ * 
+ * @jingjiejiang Aug 18, 2019
+ */
+var deleteNode = function(root, key) {
+    
+};
