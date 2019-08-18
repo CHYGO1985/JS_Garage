09ec8ac0c1888d98fa790b1e4c968cb976855415
JJ Jiang <jingjiejiang85@gmail.com>
2019-08-18 17:56:48 +0800

    refactoring

--- LeetCode/Tree/DeleteNodeInABST.js
+++ LeetCode/Tree/DeleteNodeInABST.js
@@ -14,4 +14,50 @@
  */
 var deleteNode = function(root, key) {
     
+    const deleter = node => {
+        
+        if (!node) return null;
+
+        if (!node.left && !node.right) {
+            return null;
+        } else if (node.right && node.left) {
+
+            let shift = node.right;
+            while (shift.left) {
+                shift = shift.left;
+            }
+            shift.left = node.left;
+            node.left = null;
+            
+            return node.right;
+        }
+
+        if (root.left) 
+            return node.left;
+        else 
+            return node.right;
+    };
+
+    const traverser = node => {
+
+        if (!node) return ;
+
+        if (node.val === key) {
+            node = deleter(node);
+        }
+
+        if (node.val < key) {
+            traverser(node.left);
+        } else {
+            traverser(node.right);
+        }      
+    };
+
+    if (!root) return root;
+    if (root.val === key) return deleter(root);
+
+    const shift = root;
+    traverser(shift);
+    
+    return root;
 };
