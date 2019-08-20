3755a46282a185e92162554096559ee34de526fb
JJ Jiang <jingjiejiang85@gmail.com>
2019-08-20 15:53:43 +0800

    116. Populating Next Right Pointers in Each Node: add constant space solution

--- LeetCode/Tree/PopulatingNextRightPointersInEachNode.js
+++ LeetCode/Tree/PopulatingNextRightPointersInEachNode.js
@@ -13,24 +13,39 @@
  * 
  * @jingjiejiang Aug 20, 2019
  */
-var connect = function(root) {
+// var connect = function(root) {
     
-    const res = [];
+    // const res = [];
 
-    const traverse = (node, level) => {
-        if (!node) return ;
+    // const traverse = (node, level) => {
+    //     if (!node) return ;
 
-        if (!res[level]) {
-            res[level] = node;
-        } else {
-            res[level].next = node;
-            res[level] = node;
-        }
+    //     if (res[level]) res[level].next = node;
+    //     res[level] = node;
+            
+    //     traverse(node.left, level + 1);
+    //     traverse(node.right, level + 1);
+    // }
 
-        traverse(node.left, level + 1);
-        traverse(node.right, level + 1);
-    }
+    // traverse(root, 0);
+    // return root;
+// };
 
-    traverse(root, 0);
-    return res;
+var connect = function(root) {
+
+    const cross = (left, right) => {
+
+        if (!left) return ;
+
+        left.next = right;
+        cross(left.right, right.left);
+    };
+
+    if (!root) return root;
+
+    cross(root.left, root.right);
+    connect(root.left);
+    connect(root.right);
+
+    return root;
 };
