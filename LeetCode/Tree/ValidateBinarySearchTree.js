49bebb44f9ebbad28268d30fbe403c4b221c42a0
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-08-24 21:16:22 +0800

    98. Validate Binary Search Tree

--- LeetCode/Tree/ValidateBinarySearchTree.js
+++ LeetCode/Tree/ValidateBinarySearchTree.js
@@ -9,22 +9,43 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root) {
+// var isValidBST = function(root) {
     
+//   const checkBST = node => {
+//     if (!node) return [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER, true];
+
+//     const leftRes = checkBST(node.left);
+//     const rightRes = checkBST(node.right);
+
+//     if (node.val > leftRes[1] && node.val < rightRes[0]) {
+//       return [Math.min(leftRes[0], node.val),
+//               Math.max(rightRes[1], node.val),
+//               leftRes[2] & rightRes[2]];
+//     } else {
+//       return [Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, false];
+//     }
+//   }
+
+//   return checkBST(root)[2];
+// };
+
+/* compress the res array from len 3 to len 2 */
+var isValidBST = function(root) {
+
   const checkBST = node => {
-    if (!node) return [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER, true];
+    if (!node) return [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER];
 
     const leftRes = checkBST(node.left);
     const rightRes = checkBST(node.right);
 
     if (node.val > leftRes[1] && node.val < rightRes[0]) {
       return [Math.min(leftRes[0], node.val),
-              Math.max(rightRes[1], node.val),
-              leftRes[2] & rightRes[2]];
+              Math.max(rightRes[1], node.val)];
     } else {
-      return [Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, false];
+      return [Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER]
     }
-  }
+  };
 
-  return checkBST(root)[2];
+  const res = checkBST(root);
+  return res[0] === Number.MIN_SAFE_INTEGER ? false : true;
 };
