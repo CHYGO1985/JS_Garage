b76257422df46848d68f2103fe9d40c18b3c342a
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-08-29 23:18:47 +0800

    958. Check Completeness of a Binary Tree

--- LeetCode/Tree/CheckCompletenessOfABinaryTree.js
+++ LeetCode/Tree/CheckCompletenessOfABinaryTree.js
@@ -13,20 +13,21 @@
  */
 var isCompleteTree = function(root) {
     // is left not  leave & right is null
-    // left is null & right : false
+    // left is null & right : false (this include left is null and right is a non leaf node)
+    let queue = [];
 
-    let res = true;
+    queue.push(root);
 
-    const checkComplete = node => {
-        if (!node) return true;
-
-        const isLeftLeave = checkComplete(node.left);
-        const isRightLeave = checkComplete(node.right);
-        
-        if ((!node.left && node.right) && (isLeftLeave === false && !node.right)) {
-            res &= false;
-        }
-
-        return (!node.left && !node.right) ? true : false;
+    while (queue.length > 0) {
+      const curNode = queue.shift()
+      // if pre is null and next is node, then return false
+      if (!curNode && queue[0]) return false;
+      
+      if (curNode) {
+        queue.push(curNode.left);
+        queue.push(curNode.right);
+      }
     }
+
+    return true;
 };
