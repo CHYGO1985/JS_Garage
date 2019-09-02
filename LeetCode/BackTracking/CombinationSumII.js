c63617221e11f77427a950cd2dd3dc89bef40553
JJ Jiang <jingjiejiang85@gmail.com>
2019-09-02 10:28:05 +0800

    40. Combination Sum II

--- LeetCode/BackTracking/CombinationSumII.js
+++ LeetCode/BackTracking/CombinationSumII.js
@@ -3,32 +3,63 @@
  * @param {number} target
  * @return {number[][]}
  * 
- * @jingjiejiang Jun 13, 2019
+ * @jingjiejiang Sep 2, 2019
  */
-var combinationSum2 = function (candidates, target) {
+// var combinationSum2 = function (candidates, target) {
 
-    let res = [];
-    let curComb = [];
+//     let res = [];
+//     let tmpRes = [];
+//     candidates.sort((a, b) => a - b);
 
-    const getComb = (digitsIdx, charArrIdx) => {
+//     const getCombination = (candidates, target, start) => {
 
-        if (curComb.length === digits.length) {
-            res.push(curComb.slice().join(""));
-            return;
+//         if (target < 0) return;
+//         if (target === 0) {
+//             res.push(tmpRes.slice());
+//             return;
+//         }
+
+//         for (let idx = start; idx < candidates.length; idx++) {
+//             if (target < 0) break;
+//             if (idx > start && candidates[idx] == candidates[idx - 1]) continue;
+//             tmpRes.push(candidates[idx]);
+//             getCombination(candidates, target - candidates[idx], idx + 1);
+//             tmpRes.pop();
+//         }
+//     };
+
+//     getCombination(candidates, target, 0);
+
+//     return res;
+// };
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function(candidates, target) {
+    
+    const res = [];
+    candidates.sort((a,b) => a - b);
+
+    const getCombination = (curSum, tmpRes, curIdx) => {
+        if (curSum < 0) return ;
+        if (curSum === 0) {
+            res.push(tmpRes.slice());
+            return ;
         }
 
-        for (let idx = digitsIdx; idx < digits.length; idx++) {
-            let charArr = numCharMap[digits.charAt(idx) - '0'];
-            console.log(charArr);
-            for (; charArrIdx < charArr.length; charArrIdx++) {
-                curComb.push(charArr.charAt(charArrIdx));
-                getComb(idx + 1, charArrIdx);
-                curComb.pop();
-            }
+        for (let idx = curIdx; idx < candidates.length; idx ++) {
+            if (curSum < 0) break;
+            // advoid duplicate, must be idx > curIdx not > 0
+            if (idx > curIdx && candidates[idx] === candidates[idx - 1]) continue;
+            tmpRes.push(candidates[idx]);
+            getCombination(curSum - candidates[idx], tmpRes, idx + 1);
+            tmpRes.pop();
         }
-    }
+    };
 
-    getComb(0, 0);
-
+    getCombination(target, [], 0);
     return res;
 };
