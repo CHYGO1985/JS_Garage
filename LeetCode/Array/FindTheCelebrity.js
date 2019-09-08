55c5726057dcb059dc65ed08653b2f1614a7a624
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-09-08 23:53:34 +0800

    277. Find the Celebrity

---
+++ LeetCode/Array/FindTheCelebrity.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for knows()
+ * 
+ * @param {integer} person a
+ * @param {integer} person b
+ * @return {boolean} whether a knows b
+ * knows = function(a, b) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} knows()
+ * @return {function}
+ * 
+ * @jingjiejiang Sep 8, 2019
+ */
+var solution = function(knows) {
+  /**
+   * @param {integer} n Total people
+   * @return {integer} The celebrity
+   */
+  return function(n) {
+      
+  };
+};
