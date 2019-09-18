af8151fea69990ab3e02be0301df7ecb41f51e08
JJ Jiang <jingjiejiang85@gmail.com>
2019-09-18 11:13:10 +0800

    768. Max Chunks To Make Sorted II

--- LeetCode/Array/MaxChunksToMakeSortedII.js
+++ LeetCode/Array/MaxChunksToMakeSortedII.js
@@ -6,4 +6,23 @@
  */
 var maxChunksToSorted = function(arr) {
     
+    if (arr == null || arr.length === 0) return 1;
+
+    const numStack = [];
+    
+    for (let idx = 0; idx < arr.length; idx ++) {
+        if (idx === 0 || arr[idx] >= numStack[numStack.length - 1]) {
+            numStack.push(arr[idx]);
+            continue;
+        }
+        
+        let curMax = numStack[numStack.length - 1];
+        numStack.pop(curMax);
+        while (numStack.length >= 0 && arr[idx] < numStack[numStack.length - 1]) {
+            numStack.pop();
+        }
+        numStack.push(curMax);
+    }
+
+    return numStack.length;
 };
