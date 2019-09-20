b0ce035165c3b784c94ffc0f380a50e447d287e5
JJ Jiang <jingjiejiang85@gmail.com>
2019-09-20 15:18:46 +0800

    49. Group Anagrams

---
+++ LeetCode/Hash/GroupAnagrams.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ * 
+ * @jingjiejiang Sep 20, 2019
+ */
+var groupAnagrams = function(strs) {
+    const res = [], strWordMap = {};
+    if (strs == null || strs.length === 0) return res;
+
+    for (let str of strs) {
+        const strKeyArr = Array(26).fill(0);
+        for (let idx = 0; idx < str.length; idx ++) {
+            let strKeyArrIdx = str.charCodeAt(idx) - 'a'.charCodeAt(0);
+            if (strKeyArr[strKeyArrIdx] == null) strKeyArr[strKeyArrIdx] = 0;
+            strKeyArr[strKeyArrIdx] += 1;
+        }
+        
+        const key = strKeyArr.join("");
+        if (strWordMap[key] == null) strWordMap[key] = [];
+        strWordMap[key].push(str);
+    }
+
+    return Object.values(strWordMap);
+};
