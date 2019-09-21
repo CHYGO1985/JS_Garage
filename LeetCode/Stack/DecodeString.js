7dc12d67aeb0b7a6bc5c1ed1cda1a2d4ba84b918
CHYGO1985 <jingjiejiang85@gmail.com>
2019-09-21 23:43:22 +0800

    init commit

---
+++ LeetCode/Stack/DecodeString.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {string}
+ * 
+ * @jingjiejiang Sep 21, 2019
+ */
+var decodeString = function(s) {
+    
+  if (s == null || s.length === 0) return s;
+
+  let res = [], num = [], chrs = [];
+
+  for (let idx = 0; idx < s.length; idx ++) {
+
+    let curChr = s.charAt(idx);
+    if (curChr > '0' && curChr < '9') {
+
+    }
+
+    
+  }
+
+  return 
+};
