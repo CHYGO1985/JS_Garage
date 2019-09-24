e942c6c56885962f6a66a635561dab84d858707b
JJ Jiang <jingjiejiang85@gmail.com>
2019-09-24 13:28:42 +0800

    227. Basic Calculator II

--- LeetCode/Stack/BasicCalculatorII.js
+++ LeetCode/Stack/BasicCalculatorII.js
@@ -9,35 +9,33 @@ var calculate = function(s) {
     if (s == null || s.length === 0) return undefined;
 
     // store op as temp data will be better than store number
-    let res = 0, curNum = 0, neg = false, op = '+'
+    let res = 0, curNum = 0, numStack = [], op = '+';
     for (let idx = 0; idx < s.length; idx ++) {
         const chr = s.charAt(idx);
-        switch (chr) {
-            case '+':
-                numStack.push(curNum);
-                curNum = 0;
-                neg = false;
-                break;
-            case '-':
-                numStack.push(curNum);
-                curNum = 0;
-                neg = true;
-                break;
-            case '*':
-                const leftNum = numStack.pop();
-                const rightNum = parseInt(s.charAt(++ idx));
-                const res = 
-                numStack.push(leftNum * rightNum);
-                break;
-            case '/':
-                const leftNum = numStack.pop();
-                const rightNum = parseInt(s.charAt(++ idx));
-                numStack.push(leftNum / rightNum);
-                break;
-            case ' ': break;
-            default:
-                curNum = curNum * 10 + parseInt(chr);
-                break;
+        if (chr >= '0' && chr <= '9') {
+            curNum = curNum * 10 + parseInt(chr);
+        } 
+        
+        if (((chr < '0' || chr > '9') && chr !== ' ') || (idx === s.length - 1)){
+            if (op === '+') numStack.push(curNum);
+            else if (op === '-') numStack.push(- curNum);
+            else { //(chr === '*' || chr === '/')
+                    
+                const preNum = numStack.pop();
+                let isNeg = preNum < 0 ? true : false;
+                let res = op === '*' ? Math.abs(preNum) * curNum : Math.floor( Math.abs(preNum) / curNum);
+                res = isNeg ? - res : res;
+                numStack.push(res);
+            }
+
+            curNum = 0;
+            op = chr;
         }
     }
+
+    while (numStack.length > 0) {
+        res += numStack.pop();
+    }
+
+    return res;
 };
