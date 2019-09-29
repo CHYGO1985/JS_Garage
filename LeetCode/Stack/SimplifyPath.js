17a12372390b934bb581b026aa749fd3335f6896
CHYGO1985 <jingjiejiang85@gmail.com>
2019-09-29 13:12:49 +0800

    71. Simplify Path

--- LeetCode/Stack/SimplifyPath.js
+++ LeetCode/Stack/SimplifyPath.js
@@ -5,5 +5,19 @@
  * @jingjiejiang Sep 28, 2019 
  */
 var simplifyPath = function(path) {
-    
+   
+  if (path == null || path.length === 0) return "";
+
+  const newPathElems = [];
+  const oldPathElems = path.split("/");
+
+  for (let pathElem of oldPathElems) {
+    if (newPathElems.length > 0 && pathElem === "..") {
+      newPathElems.pop();
+    } else if (pathElem !== "." && pathElem !== ".." && pathElem !== "") {
+      newPathElems.push(pathElem);
+    }
+  }
+
+  return "/" + newPathElems.join("/");
 };
