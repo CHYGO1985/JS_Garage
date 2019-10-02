68f0c8f8a43516ae4770c8b8a7411cdeb8436c75
JJ Jiang <jingjiejiang85@gmail.com>
2019-10-02 09:49:12 +0800

    151. Reverse Words in a String

---
+++ LeetCode/String/ReverseWordsInAString.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {string}
+ * 
+ * @jingjiejiang Oct 2, 2019
+ */
+var reverseWords = function(s) {
+    
+    if (s == null || s.length === 0) return s;
+
+    let words = s.split(" ");
+    let head = 0, rear = words.length - 1;
+    
+    const swap = (leftIdx, rightIdx) => {
+        const tmp = words[leftIdx];
+        words[leftIdx] = words[rightIdx];
+        words[rightIdx] = tmp;
+    }
+
+    while (head < rear) {
+        while (head < rear && words[head] === " ") head ++;
+        while (head > rear && words[rear] === " ") rear --;
+
+        swap(head ++, rear --);
+    }
+
+    return 
+};
