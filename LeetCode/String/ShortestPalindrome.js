736b3f6f89d74b474beea84665319cd63a3e1ccb
JJ Jiang <jingjiejiang85@gmail.com>
2019-10-04 22:49:32 +0800

    214. Shortest Palindrome

---
+++ LeetCode/String/ShortestPalindrome.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string}
+ * 
+ * @jingjiejiang Oct 4, 2019
+ */
+// two pointers
+var shortestPalindrome = function(s) {
+    
+    let head = 0, n = s.length();
+    for (let rear = n - 1; rear >= 0; --rear) {
+        if (s.charAt(head) == s.charAt(rear)) ++ head;
+    }
+    if (head === n) return s;
+    let rem = s.substring(head);
+    let rem_rev = new StringBuilder(rem).reverse().toString();
+    return rem_rev + shortestPalindrome(s.substring(0, head)) + rem;
+};
