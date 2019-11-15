11654f7bc5a8e326d2c88b808e22069f57482fb0
JJ Jiang <jingjiejiang85@gmail.com>
2019-11-15 16:35:06 +0800

    126. Word Ladder II: clean code: final version

--- LeetCode/BFS/WordLadderII.java
+++ LeetCode/BFS/WordLadderII.java
@@ -1,6 +1,15 @@
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 /**
  * @jingjiejiang Nov 14, 2019
  */
+/*
 public class WordLadderII {
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
         
@@ -94,5 +103,86 @@ public class WordLadderII {
             for (String s : parentsMap.get(word))
                 backTrace(s, start, tmplist, resList, parentsMap);
         tmplist.remove(0);
+    }
+}
+*/
+
+public class WordLadderII {
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        
+        List<List<String>> resList = new ArrayList<>();
+        if (wordList.size() == 0) return resList;
+
+        int minDepth = Integer.MAX_VALUE;
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+
+        // word : depth map
+        Map<String, Integer> wordDepthMap = new HashMap<>();
+        for (String word : wordList) {
+            wordDepthMap.put(word, Integer.MAX_VALUE);
+        }
+        wordDepthMap.put(beginWord, 0);
+        // BFS parents map
+        Map<String, List<String>> parentsMap = new HashMap<>();
+
+        while (queue.isEmpty() == false) {
+
+            String curWord = queue.poll();
+
+            int depth = wordDepthMap.get(curWord) + 1;
+            if (depth > minDepth) break; // as the word in the queue must >= curWord
+
+            for (int idx = 0; idx < curWord.length(); idx ++) {
+                for (char letter = 'a'; letter < 'z'; letter ++) {
+                    StringBuilder wordBuilder = new StringBuilder(curWord);
+                    wordBuilder.setCharAt(idx, letter);
+                    String newWord = wordBuilder.toString();
+
+                    // check if newWord exist in the list
+                    if (wordDepthMap.containsKey(newWord)) {
+                        if (depth > wordDepthMap.get(newWord)) continue;
+
+                        // if current depth < newWord old depth, update it
+                        if (depth < wordDepthMap.get(newWord)) {
+                            queue.add(newWord);
+                            wordDepthMap.put(newWord, depth);
+                        }
+
+                        // add newWord to the curWord parents list
+                        List<String> parentList = parentsMap.getOrDefault(newWord, new LinkedList<>());
+                        parentList.add(curWord);
+                        parentsMap.put(newWord, parentList);
+                    }
+
+                    if (newWord.equals(endWord)) minDepth = depth;
+                }
+            }
+        }
+
+        List<String> tmpList = new LinkedList<>();
+        backTrace(endWord, beginWord, tmpList, resList, parentsMap);
+        
+        return resList;
+    }
+
+    private void backTrace(String childWord, String parentWord, List<String> tmplist, 
+                           List<List<String>> resList, Map<String, List<String>> parentsMap) {
+        
+        if (childWord.equals(parentWord)) {
+            List<String> addList = new ArrayList<>(tmplist);
+            addList.add(0, childWord);
+            resList.add(addList);
+            return ;
+        }
+
+        if (parentsMap.get(childWord) != null) {
+            for (String word : parentsMap.get(childWord)) {
+                tmplist.add(0, childWord);
+                backTrace(word, parentWord, tmplist, resList, parentsMap);
+                tmplist.remove(0);
+            }
+        }
     }
 }
