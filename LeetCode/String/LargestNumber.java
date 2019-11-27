06013f04100807be598b4335f90af75c5af2ed35
CHYGO1985 <jingjiejiang85@gmail.com>
2019-11-27 23:58:04 +0800

    179. Largest Number: completed

--- LeetCode/String/LargestNumber.java
+++ LeetCode/String/LargestNumber.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * @jingjiejiang Nov 27, 2019 
  */
@@ -7,8 +9,22 @@ class Solution {
     StringBuilder builder = new StringBuilder();
     if (nums == null || nums.length == 0) return builder.toString();
 
-    
+    String[] numsInStr = new String[nums.length];
 
+    for (int idx = 0; idx < nums.length; idx ++) {
+      numsInStr[idx] = String.valueOf(nums[idx]);
+    } 
+
+    Arrays.sort(numsInStr, (a, b) -> (b + a).compareTo(a + b));
+
+    // ** for a bunch of "0"
+    if (numsInStr[0].charAt(0) == '0') return "0";
+
+    for (String num : numsInStr) {
+      builder.append(num);
+    }
+
+    return builder.toString();
   }
 }
 
