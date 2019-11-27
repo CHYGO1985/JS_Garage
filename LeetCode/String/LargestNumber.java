7d4b29dc2851039aaa52d24923487625a9ce9994
CHYGO1985 <jingjiejiang85@gmail.com>
2019-11-27 23:51:15 +0800

    179. Largest Number: init commit

---
+++ LeetCode/String/LargestNumber.java
@@ -0,0 +1,14 @@
+/**
+ * @jingjiejiang Nov 27, 2019 
+ */
+class Solution {
+  public String largestNumber(int[] nums) {
+      
+    StringBuilder builder = new StringBuilder();
+    if (nums == null || nums.length == 0) return builder.toString();
+
+    
+
+  }
+}
+
