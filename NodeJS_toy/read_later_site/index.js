ef83cbd5108079f5a6909ebfe56d52ac49e78b1b
CHYGO1985 <jingjiejiang85@gmail.com>
2022-04-22 17:24:59 +0800

    read later site
    1.add index.js
    2.add db.js for db ops
    todo: integrate db ops of db.js into index.js

--- NodeJS_toy/read_later_site/index.js
+++ NodeJS_toy/read_later_site/index.js
@@ -7,16 +7,49 @@
  * 
  */
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const articles = [{ title: 'Example' }];
 
+app.set('port', process.env.PORT || 3000);
+// supprt request bodies codede as JSON
+app.use(bodyParser.json());
+// support form-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* Front page */
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Express web app available at localhost: ${port}`);
+/* Get all articles */
+app.get('/articles', (req, res, next) => {
+  res.send(articles);
+});
+
+/* Creates an article */
+app.post('/article', (req, res, next) => {
+  const article = { title: req.body.title };
+  articles.push(article);
+  res.send(article);
+});
+
+app.get("/articles/:id", (req, res, next) => {
+  const id = req.params.id;
+  console.log('Fetching: ', id);
+  res.send(articles[id]);
+});
+
+app.delete('/articles/:id', (req, res, next) => {
+  const id = req.params.id;
+  console.log('Deleting: ', id);
+  delete articles[id];
+  res.send({message: 'Deleted'});
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Express web app available at localhost: ${app.get('port')}`);
 });
   
 
