35084243bf4eef8487d3b7ee6c58e34827dbdade
CHYGO1985 <jingjiejiang85@gmail.com>
2022-05-01 21:45:25 +0800

    es2015 with babel set up

---
+++ NodeJS_toy/es2015-example/browser.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author jingjiejiang
+ * @history May 1, 2022
+ * 
+ */
+class Example {
+  render() {
+    return '<h1>Example</h1>'
+  }
+}
+
+const example = new Example();
+console.log(example.render());
