98eb5bd4f7013508fd04c3fdb4aaabb47892f45d
CHYGO1985 <jingjiejiang85@gmail.com>
2022-05-02 13:22:33 +0800

    gulp proj

---
+++ NodeJS_toy/gulp-example/gulpfile.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * Find jsx file, then babel to process es2015 and react.
+ * 
+ * @author jingjiejiang
+ * @history May 2, 2022
+ * 
+ */
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+
+gulp.task('default', () => {
+
+  return gulp.src('app/*.jsx')  // find all Reat jsx file
+    .pipe(sourcemaps.init())    // starts watching source files to build srcmaps for debug
+    .pipe(babel({
+      presets: ['@babel/preset-env', '@babel/preset-react']  // config gulp-babel to use es2015 and React
+    }))
+    .pipe(concat('all.js'))         // concats all source files into all.js
+    .pipe(sourcemaps.write('.'))    // write src map files seperately
+    .pipe(gulp.dest('dist'));       // redirect all files to the dist/ folder
+});
