8d9eeee1e0a0283206867553bacc35321e0d3e79
CHYGO1985 <jingjiejiang85@gmail.com>
2022-05-04 16:28:35 +0800

    Add post route

--- NodeJS_toy/shoutbox/routes/entries.js
+++ NodeJS_toy/shoutbox/routes/entries.js
@@ -8,6 +8,27 @@
  * Add export form
  * 
  */
+
+const { title } = require("process");
+const Entry = require("../models/entry");
+
+// entry for submitting data 
+exports.submit = (req, res, next) => {
+  const data = req.body.entry;  // from name = entry[...]
+  const user = res.locals.user;
+  const username = user ? user.name : null;
+
+  const entry = new Entry({
+    username: username,
+    title: title,
+    body: data.body
+  });
+  entry.save((err) => {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+};
+
 exports.form = (req, res) => {
-  res.render('psot', { title: 'post' });
+  res.render('post', { title: 'post' });
 };
