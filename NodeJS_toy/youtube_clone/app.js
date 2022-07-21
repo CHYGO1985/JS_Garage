ffc161a52053ddb678ab86b0f7d133169d4f6b7d
CHYGO1985 <jingjiejiang85@gmail.com>
2022-07-21 11:55:39 +0800

    revise err route message

--- NodeJS_toy/youtube_clone/app.js
+++ NodeJS_toy/youtube_clone/app.js
@@ -50,7 +50,7 @@ app.use(notFoundRouter);
 // error handler
 app.use((err, req, res) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  res.locals.message = err.message || 'Something went wrong!';
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
