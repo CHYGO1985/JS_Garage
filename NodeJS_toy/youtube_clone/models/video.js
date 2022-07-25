58ed39e564e343a139638944371d27417fd66022
CHYGO1985 <jingjiejiang85@gmail.com>
2022-07-25 16:24:38 +0800

    add video model

---
+++ NodeJS_toy/youtube_clone/models/video.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const VideoSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      reqiured: true,
+    },
+    desc: {
+      type: String,
+      rquired: true,
+    },
+    imgUrl: {
+      type: String,
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+    likes: {
+      type: [String],
+      default: [],
+    },
+    dislike: {
+      type: [String],
+      default: [],
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model('Vedio', VideoSchema);
