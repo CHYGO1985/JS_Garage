e7cd235c2ac772c74f50783af5f0ded285a6125f
CHYGO1985 <jingjiejiang85@gmail.com>
2022-07-28 20:28:07 +0800

    add update user api

--- NodeJS_toy/youtube_clone/controllers/user-controller.js
+++ NodeJS_toy/youtube_clone/controllers/user-controller.js
@@ -2,11 +2,52 @@
 // const User = require('../models/user');
 
 import winstonLogger from '../config/winston.js';
+import User from '../models/user.js';
+import { createError } from '../utils/error.js';
 
 export const test = () => {
   winstonLogger.info('winston logger is called');
 };
 
-export const two = () => {
-  console.log('two');
+export const updateUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updateUser);
+    } catch (err) {
+
+    }
+  } else {
+    return next(createError(403, 'You can only update your account!'));
+  }
+};
+
+export const deleteUser = () => {
+
+};
+
+export const getUser = () => {
+
+};
+
+export const subscribe = () => {
+
+};
+
+export const unsubscribe = () => {
+
+};
+
+export const like = () => {
+
+};
+
+export const dislike = () => {
+
 };
--- NodeJS_toy/youtube_clone/routes/user-route.js
+++ NodeJS_toy/youtube_clone/routes/user-route.js
@@ -1,8 +1,37 @@
 import express from 'express';
-import { test } from '../controllers/user-controller.js';
+import {
+  test,
+  updateUser,
+  deleteUser,
+  getUser,
+  subscribe,
+  unsubscribe,
+  like,
+  dislike
+} from '../controllers/user-controller.js';
+import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
 router.get('/test', test);
+
+// update a user
+router.put('/:id', verifyToken, updateUser);
+
+// delete a user
+router.delete('/:id', verifyToken, deleteUser);
+
+// get the info of a user
+router.get('/find/:id', getUser);
+
+// subscribe a user
+router.put('/sub/:id', verifyToken, subscribe);
+
+// unsunscribe a user
+// ??? why use put not delete?
+
+// like a video
+
+// dislike a video
 
 export default router;
