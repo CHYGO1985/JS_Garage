9efdc857a61220ca35c91299dd90b006de23bf88
CHYGO1985 <jingjiejiang85@gmail.com>
2022-07-30 23:07:14 +0800

    add more test case for auth signup api

--- NodeJS_toy/youtube_clone/test/unit/auth-route.test.js
+++ NodeJS_toy/youtube_clone/test/unit/auth-route.test.js
@@ -28,15 +28,43 @@ describe('POST signup', () => {
     expect(text).to.be.equal('User has been created!')
   });
 
-  it('post duplicate user info and respone with 500 and a message showes that duplicate key error', async () => {
+  const dupUserName = {
+    name: 'test11',
+    email: 'test22@gmail.com',
+    password: '1234'
+  };
+  it('post duplicate user name and respone with 500 and a msg showes that duplicate key error', async () => {
     const { text, status } = await request(app)
       .post('/api/auth/signup')
       .set('Accept', 'application/json')
-      .send(tmpUser);
+      .send(dupUserName);
+    expect(status).to.be.equal(500);
+    expect(text).to.contains('E11000 duplicate key error collection');
+  });
+
+  const dupUserEmail = {
+    name: 'test22',
+    email: 'test11@gmail.com',
+    password: '1234'
+  };
+  it('post duplicate user email and respone with 500 and a msg showes that duplicate key error', async () => {
+    const { text, status } = await request(app)
+      .post('/api/auth/signup')
+      .set('Accept', 'application/json')
+      .send(dupUserName);
     expect(status).to.be.equal(500);
     expect(text).to.contains('E11000 duplicate key error collection');
 
     // clean user db
     await User.deleteMany();
+  });
+});
+
+/**
+ * Test /api/auth/signin endpoint
+ */
+describe('POST signin', () => {
+  it('post valid username and password and response with 200 and username and user email', async () => {
+
   });
 });
