f69ca0abd5304401708cf989168cc429adb3786f
CHYGO1985 <jingjiejiang85@gmail.com>
2022-08-07 19:27:12 +0800

    add Menu and Navbar components

--- NodeJS_toy/youtube_clone_client/src/App.js
+++ NodeJS_toy/youtube_clone_client/src/App.js
@@ -1,9 +1,33 @@
-function App() {
+import React from 'react';
+import styled from 'styled-components';
+
+import Menu from './Components/Menu';
+import Navbar from './Components/Navbar';
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 7;
+`;
+
+const Wrapper = styled.div`
+
+`;
+
+const App = () => {
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>
+          video cards
+        </Wrapper>
+      </Main>
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ NodeJS_toy/youtube_clone_client/src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      navbar
+    </Container>
+  );
+};
+
+export default Navbar;
