c8edeacbf82067e98d34cd05672cbda853f5095d
CHYGO1985 <jingjiejiang85@gmail.com>
2022-08-07 22:42:03 +0800

    add logo and home button in menu item

--- NodeJS_toy/youtube_clone_client/src/Components/Menu.js
+++ NodeJS_toy/youtube_clone_client/src/Components/Menu.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import HomeIcon from '@mui/icons-material/Home';
+
 const Container = styled.div`
   flex: 1;
   background-color: #202020;
@@ -8,10 +10,46 @@ const Container = styled.div`
   color: white;
 `;
 
+const Wrapper = styled.div`
+  padding: 18px 26px;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-weight: bold;
+  margin-bottom: 25px;
+`;
+
+const Img = styled.img`
+  height: 25px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  cursor: pointer;
+  padding: 7.5px 0px;
+`;
+
 const Menu = () => {
   return (
     <Container>
-      Menu
+      <Wrapper>
+        <Logo>
+          <Img src='/images/logo.png' />
+          JingjieTube
+        </Logo>
+        <Item>
+          <HomeIcon />
+          Home
+        </Item>
+        <Item>
+          Explore
+        </Item>
+      </Wrapper>
     </Container>
   );
 };
