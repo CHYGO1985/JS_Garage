356602c470fa0fe540b0f24a4a122478d1367e1b
CHYGO1985 <jingjiejiang85@gmail.com>
2022-08-11 20:23:22 +0800

    complete nav bar

--- NodeJS_toy/youtube_clone_client/src/Components/Navbar.js
+++ NodeJS_toy/youtube_clone_client/src/Components/Navbar.js
@@ -1,38 +1,74 @@
 import React from 'react';
 import styled from 'styled-components';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Container = styled.div`
   background-color: ${({ theme }) => theme.bg};
+  position: sticky;
+  top: 0;
+  height: 56px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  flex-direction: row;
+  height: 100%;
+  padding: 0px 20px;
+  position: relative;
+`;
+
+const Search = styled.div`
+  width: 400px;
+  position: absolute;
+  left: 0px;
+  right: 0px;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 10px;
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+`;
+
+const Input = styled.input`
+  border: none;
+  background-color: transparent;
+  outline: none;
+  color: ${({ theme }) => theme.textColor};
+`;
+
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  color: #3ea6ff;
+  border: 1px solid #3ea6ff;
+  align-items: center;
+  display: flex;
+  border-radius: 5px;
+  margin-top: 10px;
+  cursor: pointer;
+  font-weight: 500;
+  gap: 5px;
 `;
 
 const Navbar = () => {
   return (
     <Container>
-      navbar
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
+      <Wrapper>
+        <Search>
+          <Input placeholder="Search" />
+          <SearchOutlinedIcon />
+        </Search>
+        <Button>
+          <AccountCircleIcon />
+          SIGN IN
+        </Button>
+      </Wrapper>
     </Container>
   );
 };
